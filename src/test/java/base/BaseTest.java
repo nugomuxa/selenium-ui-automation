843d92cb44bb4a503b50b29ddc79dcf41313f206
nugomuxa <nugomukhigulashvili99@gmail.com>
2025-12-14 23:12:47 +0400

    Fix CI failures by  disabling Chrome password manager and running headless

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,31 +6,31 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-
         ChromeOptions options = new ChromeOptions();
 
-        // 🔥 INCOGNITO = no password popup
-        options.addArguments("--incognito");
-
-        // Disable password manager
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("profile.password_manager_enabled", false);
-        options.setExperimentalOption("prefs", prefs);
-
-        // Extra safety
+        //
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-infobars");
+        options.addArguments("--disable-extensions");
+
+        //  PASSWORD MANAGER OFF
         options.addArguments("--disable-save-password-bubble");
+        options.setExperimentalOption(
+                "prefs",
+                java.util.Map.of(
+                        "credentials_enable_service", false,
+                        "profile.password_manager_enabled", false
+                )
+        );
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
